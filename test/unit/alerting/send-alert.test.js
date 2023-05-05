1173f2729aab1c5f2b18759a3dff94cbfe81ad70
John Watson <johnwatson484@hotmail.com>
2023-05-05 23:17:52 +0100

    Add tests

---
+++ test/unit/alerting/send-alert.test.js
@@ -0,0 +1,38 @@
+jest.mock('notifications-node-client')
+const { NotifyClient: MockNotifyClient } = require('notifications-node-client')
+
+jest.mock('../../../app/alerting/get-personalisation')
+const { getPersonalisation: mockGetPersonalisation } = require('../../../app/alerting/get-personalisation')
+
+const { EMAIL } = require('../../mocks/values/email')
+const event = require('../../mocks/events/event')
+
+const { PAYMENT_REJECTED } = require('../../../app/constants/templates')
+
+const { sendAlert } = require('../../../app/alerting/send-alert')
+
+describe('send alert', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    mockGetPersonalisation.mockReturnValue({ test: 'value' })
+  })
+
+  test('should send alert via notify', async () => {
+    await sendAlert(EMAIL, PAYMENT_REJECTED, event)
+    expect(MockNotifyClient.prototype.sendEmail).toHaveBeenCalledTimes(1)
+  })
+
+  test('should get personalisation from event', async () => {
+    await sendAlert(EMAIL, PAYMENT_REJECTED, event)
+    expect(mockGetPersonalisation).toHaveBeenCalledWith(event)
+  })
+
+  test('should send alert to recipient with template and personalisation', async () => {
+    await sendAlert(EMAIL, PAYMENT_REJECTED, event)
+    expect(MockNotifyClient.prototype.sendEmail).toHaveBeenCalledWith(
+      PAYMENT_REJECTED,
+      EMAIL,
+      { personalisation: { test: 'value' } }
+    )
+  })
+})

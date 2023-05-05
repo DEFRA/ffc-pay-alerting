72173e6303fd164999dd190aaed159b864c9d8a2
John Watson <johnwatson484@hotmail.com>
2023-05-05 22:32:14 +0100

    Add tests

---
+++ test/unit/alerting/get-scheme.test.js
@@ -0,0 +1,59 @@
+const {
+  SFI,
+  SFIP,
+  LUMP_SUMS,
+  VET_VISITS,
+  CS,
+  BPS,
+  FDMR,
+  MANUAL
+} = require('../../../app/constants/schemes')
+
+const { getScheme } = require('../../../app/alerting/get-scheme')
+
+describe('get scheme', () => {
+  test('should return SFI name for SFI scheme', () => {
+    const result = getScheme(SFI)
+    expect(result).toBe('SFI')
+  })
+
+  test('should return SFI Pilot name for SFI Pilot scheme', () => {
+    const result = getScheme(SFIP)
+    expect(result).toBe('SFI Pilot')
+  })
+
+  test('should return Lump Sums name for Lump Sums scheme', () => {
+    const result = getScheme(LUMP_SUMS)
+    expect(result).toBe('Lump Sums')
+  })
+
+  test('should return Vet Visits name for Vet Visits scheme', () => {
+    const result = getScheme(VET_VISITS)
+    expect(result).toBe('Vet Visits')
+  })
+
+  test('should return Countryside Stewardship name for Countryside Stewardship scheme', () => {
+    const result = getScheme(CS)
+    expect(result).toBe('Countryside Stewardship')
+  })
+
+  test('should return BPS name for BPS scheme', () => {
+    const result = getScheme(BPS)
+    expect(result).toBe('BPS')
+  })
+
+  test('should return FDMR name for FDMR scheme', () => {
+    const result = getScheme(FDMR)
+    expect(result).toBe('FDMR')
+  })
+
+  test('should return Manual Invoice name for Manual Invoice scheme', () => {
+    const result = getScheme(MANUAL)
+    expect(result).toBe('Manual Invoice')
+  })
+
+  test('should return unknown for unknown scheme', () => {
+    const result = getScheme('LNR')
+    expect(result).toBe('Unknown')
+  })
+})

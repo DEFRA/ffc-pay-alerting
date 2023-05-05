5385d05295a0ead437e0d6dec49227debf97909d
John Watson <johnwatson484@hotmail.com>
2023-05-05 23:08:24 +0100

    Add tests

---
+++ test/unit/alerting/send-alerts.test.js
@@ -0,0 +1,25 @@
+jest.mock('../../../app/alerting/send-alert')
+const { sendAlert: mockSendAlert } = require('../../../app/alerting/send-alert')
+
+const { EMAIL } = require('../../mocks/values/email')
+const event = require('../../mocks/events/event')
+
+const { PAYMENT_REJECTED } = require('../../../app/constants/templates')
+
+const { sendAlerts } = require('../../../app/alerting/send-alerts')
+
+describe('send alerts', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  test('should send alert for each recipient', async () => {
+    await sendAlerts([EMAIL, EMAIL], PAYMENT_REJECTED, event)
+    expect(mockSendAlert).toHaveBeenCalledTimes(2)
+  })
+
+  test('should send alert with recipient, template and event', async () => {
+    await sendAlerts([EMAIL, EMAIL], PAYMENT_REJECTED, event)
+    expect(mockSendAlert).toHaveBeenCalledWith(EMAIL, PAYMENT_REJECTED, event)
+  })
+})

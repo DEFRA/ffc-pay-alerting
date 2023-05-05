24e3b0b4d86bd6fc64aadfd6add64e03ffea0da0
John Watson <john.watson1@defra.gov.uk>
2023-05-05 11:32:33 +0100

    Add recipients

---
+++ app/alerting/get-email-addresses.js
@@ -0,0 +1,47 @@
+const {
+  BATCH_REJECTED,
+  BATCH_QUARANTINED,
+  PAYMENT_REJECTED,
+  PAYMENT_DAX_REJECTED,
+  PAYMENT_INVALID_BANK,
+  PAYMENT_PROCESSING_FAILED,
+  PAYMENT_SETTLEMENT_UNMATCHED,
+  RESPONSE_REJECTED,
+  PAYMENT_REQUEST_BLOCKED
+} = require('./events')
+
+const { BATCH_PROCESSOR } = require('../constants/sources')
+
+const { alertConfig } = require('../config')
+
+const getEmailAddresses = (eventType, eventSource) => {
+  switch (eventType) {
+    case BATCH_REJECTED:
+      return `${alertConfig.coreSolutionsTeamEmails};${alertConfig.devTeamEmails}`
+    case BATCH_QUARANTINED:
+      return `${alertConfig.devTeamEmails}`
+    case PAYMENT_REJECTED:
+      if (eventSource === BATCH_PROCESSOR) {
+        return `${alertConfig.coreSolutionsTeamEmails};${alertConfig.devTeamEmails}`
+      }
+      return `${alertConfig.devTeamEmails}`
+    case PAYMENT_DAX_REJECTED:
+      return `${alertConfig.devTeamEmails}`
+    case PAYMENT_INVALID_BANK:
+      return `${alertConfig.invalidBankDetailsEmails}`
+    case PAYMENT_PROCESSING_FAILED:
+      return `${alertConfig.devTeamEmails}`
+    case PAYMENT_SETTLEMENT_UNMATCHED:
+      return `${alertConfig.devTeamEmails}`
+    case RESPONSE_REJECTED:
+      return `${alertConfig.devTeamEmails}`
+    case PAYMENT_REQUEST_BLOCKED:
+      return `${alertConfig.debtEnrichmentEmails}`
+    default:
+      return undefined
+  }
+}
+
+module.exports = {
+  getEmailAddresses
+}
--- app/alerting/get-recipients.js
+++ app/alerting/get-recipients.js
@@ -1,5 +1,8 @@
+const { getEmailAddresses } = require('./get-email-addresses')
+
 const getRecipients = (event) => {
-  
+  const emailAddresses = getEmailAddresses(event.type, event.source)
+  return emailAddresses ? emailAddresses.split(';') : []
 }
 
 module.exports = {

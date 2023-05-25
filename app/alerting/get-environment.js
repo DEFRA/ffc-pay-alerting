366ed8a994f195d59dee850dacfa67f375ad7cad
John Watson <john.watson1@defra.gov.uk>
2023-05-25 14:01:40 +0100

    Add sandpit references

--- app/alerting/get-environment.js
+++ app/alerting/get-environment.js
@@ -1,12 +1,14 @@
 const { alertConfig } = require('../config')
-const { LOCAL, DEVELOPMENT, TEST, PRE_PRODUCTION, PRODUCTION } = require('../constants/environment-codes')
-const { LOCAL_NAME, DEVELOPMENT_NAME, TEST_NAME, PRE_PRODUCTION_NAME, PRODUCTION_NAME } = require('../constants/environment-names')
+const { LOCAL, SANDPIT, DEVELOPMENT, TEST, PRE_PRODUCTION, PRODUCTION } = require('../constants/environment-codes')
+const { LOCAL_NAME, SANDPIT_NAME, DEVELOPMENT_NAME, TEST_NAME, PRE_PRODUCTION_NAME, PRODUCTION_NAME } = require('../constants/environment-names')
 const { UNKNOWN } = require('../constants/unknown')
 
 const getEnvironment = () => {
   switch (alertConfig.environment) {
     case LOCAL:
       return LOCAL_NAME
+    case SANDPIT:
+      return SANDPIT_NAME
     case DEVELOPMENT:
       return DEVELOPMENT_NAME
     case TEST:
--- app/constants/environment-codes.js
+++ app/constants/environment-codes.js
@@ -1,5 +1,6 @@
 module.exports = {
   LOCAL: 'local',
+  SANDPIT: 'snd',
   DEVELOPMENT: 'dev',
   TEST: 'test',
   PRE_PRODUCTION: 'pre',

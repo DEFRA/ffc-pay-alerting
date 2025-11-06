539b933d38f0c740d957eba34ebca31a43cd73f4
Sam Plackett <samuel.plackett@eviden.com>
2025-11-06 16:04:20 +0000

    migrate to ffc-database

--- app/data/index.js
+++ app/data/index.js
@@ -1,30 +1,9 @@
-const fs = require('node:fs')
 const path = require('node:path')
-const { Sequelize, DataTypes } = require('sequelize')
+const { Database } = require('ffc-database')
 const { databaseConfig } = require('../config')
 const modelPath = path.join(__dirname, 'models')
-const db = {}
 
-const sequelize = new Sequelize(databaseConfig.database, databaseConfig.username, databaseConfig.password, databaseConfig)
-
-const extensionLengthToTrim = -3
-
-const files = fs.readdirSync(modelPath).filter(file => {
-  return (file.indexOf('.') !== 0) && (file !== 'index.js') && (file.slice(extensionLengthToTrim) === '.js')
-})
-
-for (const file of files) {
-  const model = require(path.join(modelPath, file))(sequelize, DataTypes)
-  db[model.name] = model
-}
-
-for (const modelName of Object.keys(db)) {
-  if (db[modelName].associate) {
-    db[modelName].associate(db)
-  }
-}
-
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+const database = new Database({ ...databaseConfig, modelPath })
+const db = database.connect()
 
 module.exports = db

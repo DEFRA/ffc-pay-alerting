a3233ad5c0615633b14c4b274db8df74d8950b21
Sam Plackett <samuel.plackett@eviden.com>
2026-02-12 17:36:16 +0000

    rebase migration commits

--- test/integration/narrow/send-alert.test.js
+++ test/integration/narrow/send-alert.test.js
@@ -1,11 +1,14 @@
-jest.mock('notifications-node-client')
-const { NotifyClient: MockNotifyClient } = require('notifications-node-client')
-
 jest.mock('../../../app/cache', () => ({
   getAlertCacheKey: jest.fn(() => 'test-key'),
   getCachedAlertMessage: jest.fn(() => undefined),
   setCachedAlertMessage: jest.fn()
 }))
+
+jest.mock('../../../app/alerting')
+const { processAlert } = require('../../../app/alerting')
+
+jest.mock('../../../app/config')
+const { alertConfig } = require('../../../app/config')
 
 const message = require('../../mocks/message')
 const { processAlertMessage } = require('../../../app/messaging/process-alert-message')
@@ -15,9 +18,15 @@ const receiver = {
 }
 
 describe('send alert', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+    alertConfig.sendAlerts = true
+    processAlert.mockResolvedValue()
+  })
+
   test('should send alert via notify', async () => {
     await processAlertMessage(message, receiver)
-    expect(MockNotifyClient.prototype.sendEmail).toHaveBeenCalled()
+    expect(processAlert).toHaveBeenCalled()
   })
 
   test('should complete message', async () => {
